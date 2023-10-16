e6caa3ef90c2b42cfaa91770ea8ad35d865d3ec3
Godofwar55 <138196053+Godofwar55@users.noreply.github.com>
2023-10-16 13:02:06 +1300

    Update maincode.py

--- maincode.py
+++ maincode.py
@@ -15,9 +15,9 @@ import color
 import color_sensor
 import distance_sensor
 from app import music
-async def trying():
+async def Main():
     motor_pair.pair(motor_pair.PAIR_1, port.A, port.E)
-    music.play_drum(2)
+    # music.play_drum(2)
     # await motor.run_for_degrees(port.C, 150, (540))
     await motor.run_for_degrees(port.D, 150, (580))
     await motor.run_for_degrees(port.E, 100, (150))
@@ -30,19 +30,15 @@ async def trying():
     await motor.run_for_degrees(port.D, 150, (580)) # mission 2
     await motor_pair.move_for_degrees(motor_pair.PAIR_1, (500), 0)
     await motor.run_for_degrees(port.E, 90, (150))
-    await motor_pair.move_for_degrees(motor_pair.PAIR_1, (200), 0)
+    await motor_pair.move_for_degrees(motor_pair.PAIR_1, (193), 0)
     await motor.run_for_degrees(port.E, 150, (150))
-    await motor_pair.move_for_degrees(motor_pair.PAIR_1, (89), 0)
-    await motor.run_for_degrees(port.D, 200, (-100))
-    motor_pair.move_for_degrees(motor_pair.PAIR_1, (-3), 0)
-    await motor_pair.move_for_degrees(motor_pair.PAIR_1, (-10), 0)
+    await motor_pair.move_for_degrees(motor_pair.PAIR_1, 90, 0, velocity=540)
+    await motor.run_for_degrees(port.D, -150, 540)
     await runloop.sleep_ms(1000)
     await motor_pair.move_for_degrees(motor_pair.PAIR_1, (-50), 0)
     await motor.run_for_degrees(port.D, 150, (580))
     await motor_pair.move_for_degrees(motor_pair.PAIR_1, (50), 0)
-    await motor.run_for_degrees(port.D, 200, (-100))
-    motor_pair.move_for_degrees(motor_pair.PAIR_1, (-3), 0)
-    await motor_pair.move_for_degrees(motor_pair.PAIR_1, (-10), 0)
+    await motor.run_for_degrees(port.D, -150, 540)
     await runloop.sleep_ms(1000)
     await motor_pair.move_for_degrees(motor_pair.PAIR_1, (-50), 0)
     await motor.run_for_degrees(port.D, 150, (580)) # mission 3
@@ -51,12 +47,16 @@ async def trying():
     await motor.run_for_degrees(port.E,240, (-150))
     await motor_pair.move_for_degrees(motor_pair.PAIR_1, (460), 0)
     await motor.run_for_degrees(port.A,315, (-300))
-    await motor_pair.move_for_degrees(motor_pair.PAIR_1, (-80), 0)
+    await motor_pair.move_for_degrees(motor_pair.PAIR_1, (-100), 0)
     await motor.run_for_degrees(port.C,170, (-700))
     await motor.run_for_degrees(port.C,-100, (700))
     await motor.run_for_degrees(port.D, 160, (-100))
-    await motor.run_for_degrees(port.C,100, (700))
+    await motor.run_for_degrees(port.C,150, (700))
+    await motor.run_for_degrees(port.A,315, (300)) # mission 4
+    await motor_pair.move_for_degrees(motor_pair.PAIR_1, (300), 0)
+    await motor.run_for_degrees(port.E, 100, (230))
+    await motor_pair.move_for_degrees(motor_pair.PAIR_1, (240), 0)
+    await motor.run_for_degrees(port.A,-100, (360))
+    await motor_pair.move_for_degrees(motor_pair.PAIR_1, (150), 0)
 
-    
-
-runloop.run(trying())
+runloop.run(Main())
