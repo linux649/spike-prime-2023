e4d6631471ebbe67359bb1764cc772ab7d773d5e
linux-coding649 <104065079+linux-coding649@users.noreply.github.com>
2023-10-18 12:51:48 +1300

    Update maincode.py
    To Y7 Part added,
    Added Bugfixes.

--- maincode.py
+++ maincode.py
@@ -1,6 +1,5 @@
 # from spike import PrimeHub, Lightmatrix, Button, statusLight, ForceSensor, notionsensor, Speaker, ColorSensor, App, DistanceSensor, Motor, MotorPair
 # from spike.contral import waith_for_seconds, wait_until, Tier
-
 from hub import light_matrix
 from hub import light
 import runloop
@@ -22,7 +21,7 @@ async def Main():
     await motor.run_for_degrees(port.D, 150, (580))
     await motor.run_for_degrees(port.E, 100, (150))
     await motor_pair.move_for_degrees(motor_pair.PAIR_1, (50), 0)
-    await motor.run_for_degrees(port.A,  -100, (150))
+    await motor.run_for_degrees(port.A,-100, (150))
     await motor_pair.move_for_degrees(motor_pair.PAIR_1, (280), 0)
     await motor.run_for_degrees(port.D, 150, (-220))
     await motor_pair.move_for_degrees(motor_pair.PAIR_1, (-400), 0)
@@ -40,7 +39,7 @@ async def Main():
     await motor_pair.move_for_degrees(motor_pair.PAIR_1, (50), 0)
     await motor.run_for_degrees(port.D, -150, 540)
     await runloop.sleep_ms(1000)
-    await motor_pair.move_for_degrees(motor_pair.PAIR_1, (-50), 0)
+    await motor_pair.move_for_degrees(motor_pair.PAIR_1, (-60), 0)
     await motor.run_for_degrees(port.D, 150, (580)) # mission 3
     await motor.run_for_degrees(port.E,150, (-150))
     await motor_pair.move_for_degrees(motor_pair.PAIR_1, (250), 0)
@@ -56,7 +55,12 @@ async def Main():
     await motor_pair.move_for_degrees(motor_pair.PAIR_1, (300), 0)
     await motor.run_for_degrees(port.E, 100, (260))
     await motor_pair.move_for_degrees(motor_pair.PAIR_1, (240), 0)
-    await motor.run_for_degrees(port.A,-100, (360))
+    await motor.run_for_degrees(port.A,-110, (360))
     await motor_pair.move_for_degrees(motor_pair.PAIR_1, (-100), 0)
+    #await motor.run_for_degrees(port.E, 45, 360) # to y7
+    #await motor_pair.move_for_degrees(motor_pair.PAIR_1, 360, 0)
+    #await motor.run_for_degrees(port.E, 45, 360)
+    #await motor_pair.move_for_degrees(motor_pair.PAIR_1, 720, 0)
+
 
 runloop.run(Main())
